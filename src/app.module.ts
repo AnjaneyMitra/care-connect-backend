3539205bb07c7754ab3e952ce1a24b9aecb6780f
AnjaneyMitra <mitraanjaney@gmail.com>
2025-11-21 10:09:54 +0530

    Implement Feature 7: Messaging System (Chat)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { LocationModule } from './location/location.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { LocationModule } from './location/location.module';
     UsersModule,
     PrismaModule,
     LocationModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/chat/chat.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
+import { ChatGateway } from './chat.gateway';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+    imports: [
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET') || 'secretKey',
+                signOptions: { expiresIn: '60m' },
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [ChatController],
+    providers: [ChatService, ChatGateway],
+    exports: [ChatService],
+})
+export class ChatModule { }
